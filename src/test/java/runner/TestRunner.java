e8eaf7a0d3edbce651ad2e970e3f9601acd8fd52
Rushikesh Bhagwat <bhagwatrushikeshm@gmail.com>
2025-03-08 16:38:00 +0530

    Updated YAML file for configuration of reports

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,10 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/FeatureFiles",glue={"StepDefinitions"},monochrome=true,plugin= {"pretty","html:target/cucumber-reports"},publish=true)
+@CucumberOptions(features="src/test/java/FeatureFiles",glue={"StepDefinitions"},  plugin = {"pretty",
+		"json:target/cucumber-report.json",          // This generates the JSON report (optional)
+		"junit:target/cucumber-results.xml"          // This generates the JUnit XML report (essential)
+},monochrome=true,publish=true)
 public class TestRunner extends AbstractTestNGCucumberTests{
 	@Override
     @DataProvider(parallel=true)
